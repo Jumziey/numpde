e476e3442669d582df2ae0b52b821ddd23b0649e
Jesper Vesterberg <jumziey@gmail.com>
2015-02-08 17:45:57 +0100

    CHanged some name and finished script

---
+++ lab 3/ex1/L2Load1D.m
@@ -0,0 +1,10 @@
+function b = L2Load1D(x,f)
+	n = length(x)-1; 
+	b = zeros(n+1,1);
+	%Using trapezoidal rule
+	for i = 1:n
+		h = x(i+1) - x(i);
+		b(i) = b(i) + f(x(i))*h/2;
+		b(i+1) = f(x(i+1))*h/2 + b(i+1); %Do we need b(i+1)? i cant see why
+	end
+end
--- lab 3/ex1/l2proj.m
+++ lab 3/ex1/l2proj.m
@@ -1,11 +1,11 @@
 clear all; close all; 
 
-f = @(x) = x*(1-x);
+f = @(x) x*(1-x);
 
 n = 10;
 h = 1/n;
-x = 0:h:n;
+x = 0:h:1;
 M = L2Mass1D(x);
-b = L2Load1D(x, @f);
-Pf = M\b;
+b = L2Load1D(x,f);
+Pf = (M\b)';
 plot(x,Pf);
