bd04d01d0cdbf25aca8f81ddf78afd9261fed8e3
Jumziey <jumziey@gmail.com>
2015-02-12 18:23:05 +0100

    Continued with Errors
    Finally starting to understand what is going on, in stark contrast to
    what i tought we are NOT to touch posterior error, which seem to be the
    only relevant error for me personally.

--- lab 3/ex2/solve.m
+++ lab 3/ex2/solve.m
@@ -1,16 +1,24 @@
 clear all; close all; 
 
-for N = [2 4 16 256]
-	h = 1/N;
-	x = 0:h:1;
-	f = @(x) 2;
-	A = stiffAssembly(x);
-	b = loadAssembly(x,f);
+N= 16;
+h = 1/N;
+x = (0:h:1);
+f = @(x) 2;
+A = stiffAssembly(x);
+b = loadAssembly(x,f);
 
-	Uh = A\b;
-	plot(x,Uh)
-	hold on
-end
+psi = A\b;
+plot(x,psi) %We do not plot with hat basis since it's basically one for every psi value. 
+hold on
 
-legend('2 intervals','4 intervals','16 intervals','256 intervals')
-title('Solving the System with -u'''' = 2')
+figure(2) %Now we need some value for the norm of error
+%Energy norm value
+%A(1,1) = 32;
+%A(end,end) = 32;
+Enorm = -1+4/3 - psi'*A*psi %WOOPA!
+n1 = ones(size(psi));
+L2norm = sum(psi.^2 .* (n1-n1./h).^2.*h)
+
+
+%legend('16 intervals')
+%title('Solving the System with -u'''' = 2')
